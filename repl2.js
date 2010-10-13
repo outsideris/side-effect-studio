0695c8ff1963c12a59f681ac3a9f8f8be9cad837
Outsider <outsideris@gmail.com>
2010-10-13 23:03:29 +0900

    node-mongodb-native 사용코드 추가

--- repl2.js
+++ repl2.js
@@ -2,8 +2,11 @@ var sys = require('sys'),
 fs = require('fs'),
 qs = require('querystring'),
 url = require('url'),
+mongodb = require('mongodb'),
 buffered_cmd = '',
 repl2 = exports,
+Db = mongodb.Db,
+Server = mongodb.Server,
 trimmer = /^\s*(.+)\s*$/m,
 scopedVar = /^\s*var\s*([_\w\$]+)(.*)$/m,
 scopeFunc = /^\s*function\s*([_\w\$]+)/,
@@ -12,73 +15,85 @@ putsm = [];
 exports.scope = {};
 
 repl2.readLine = function(_cmd, uid) {
+    sys.debug("start readLine");
 	if (!exports.scope[uid]) {
 		exports.scope[uid] = {};
 	}
 
-	var cmd = trimWhitespace(_cmd),
+    var cmd = trimWhitespace(_cmd),
 	parsedKeyword = parseREPLKeyword(cmd, uid),
 	output = [],
 	output_tmp,
 	output_s;
 
 	if (parsedKeyword) {
+        sys.debug("is paredKeyword");
 		return ['|'];
 	}
 
     var isUserCommand = userCommand(cmd, uid)
     if (isUserCommand) {
         var output = [];
-        sys.puts(cmd);
-        switch (cmd) {
-            case "whoami()":
-               output.push("Outsider. Front-end & Server-side Web developer");
-               return output;
-            case "contacts()":
-               output.push("Blog: http://blog.outsider.ne.kr");
-               output.push("email: outsideris@gmail.com");
-               return output
-            case "projects()":
-               output.push("HotFlow: Scala Project");
-               return output;
-            case "skills()":
-               output.push("Java, Scala, Javascript, HTML, CSS, node.js, Spring, jQuery, MongDB");
-               return output;
-        }
+        cmd = stripBrace(cmd); 
+        sys.debug("user command: " + cmd);
+
+        var db = new Db('sideeffect_main', new Server("127.0.0.1", 27017, {}));
+        db.open(function(err, db) {
+            db.collection("trynode", function(err, collection) {
+               collection.find({"cmd":cmd}, function(err, cursor) {
+                  sys.debug("before each");
+                  cursor.each(function(err, item) {
+                     if (item != null) {
+                         output.push(item.contents); 
+                         sys.debug("contents: " + sys.inspect(item.contents));
+                     } else {
+                         sys.debug("end");
+                         return output;
+                     }
+                  }); 
+                  sys.debug("output: " + output);
+                  //return output;
+              });   
+           });        
+        });
+        output.push("test");
+        //return output;
+    } else {
+
+	    buffered_cmd += _cmd;
+	    buffered_cmd = convertToScope(buffered_cmd, uid);
+
+	    try {
+		    with(exports.scope[uid]) {
+			    output = [];
+                //sys.puts(buffered_cmd);
+			    output_tmp = eval(buffered_cmd);
+			    output_s = sys.inspect(output_tmp); // otherwise foo = {} will notput {} properly
+			    if (output_tmp) {
+				    output.push(output_s);
+			    }
+			    if (output_tmp !== undefined) {
+				    exports.scope[uid]['_'] = output_s;
+			    }
+		    }
+		    buffered_cmd = '';
+	    } catch(e) {
+	    	if (e instanceof SyntaxError) {
+			    output.push('SyntaxError');
+		    } else {
+			    output.push("Error: " + e.stack.split("\n")[0]);
+			    buffered_cmd = '';
+		    }
+	    }
+
+    	if (putsm.length > 0) {
+		    output = output.concat(putsm);
+		    extra_output = ''
+		    putsm = [];
+	    }
+	    return output;
     }
-
-	buffered_cmd += _cmd;
-	buffered_cmd = convertToScope(buffered_cmd, uid);
-
-	try {
-		with(exports.scope[uid]) {
-			output = [];
-      //sys.puts(buffered_cmd);
-			output_tmp = eval(buffered_cmd);
-			output_s = sys.inspect(output_tmp); // otherwise foo = {} will notput {} properly
-			if (output_tmp) {
-				output.push(output_s);
-			}
-			if (output_tmp !== undefined) {
-				exports.scope[uid]['_'] = output_s;
-			}
-		}
-		buffered_cmd = '';
-	} catch(e) {
-		if (e instanceof SyntaxError) {
-			output.push('SyntaxError');
-		} else {
-			output.push("Error: " + e.stack.split("\n")[0]);
-			buffered_cmd = '';
-		}
-	}
-
-	if (putsm.length > 0) {
-		output = output.concat(putsm);
-		extra_output = ''
-		putsm = [];
-	}
-	return output;
+    sys.debug("end of readLine");
 };
 
 var trimWhitespace = function(cmd) {
@@ -121,19 +136,23 @@ function parseREPLKeyword(cmd, uid) {
 }
 
 var userCommand = function(cmd) {
+    cmd = stripBrace(cmd); 
     switch (cmd) {
-       case "whoami()":
+       case "whoami":
           return true;
-       case "contacts()":
+       case "contacts":
           return true;
-       case "projects()":
+       case "projects":
           return true;
-       case "skills()":
+       case "skills":
           return true;
     }
     return false;
 }
 
+var stripBrace = function(cmd) {
+   return cmd.replace(/\(\)$/, "");
+}
 //sys.puts("Server at http://" + HOST + ':' + PORT.toString() + '/');
 
 sys.puts = function(msg) {
