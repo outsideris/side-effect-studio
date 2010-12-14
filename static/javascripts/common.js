5f00e36ddb76a8545d698b52d6a3cd9b4f6435fe
Outsider <outsideris@gmail.com>
2010-12-15 00:34:09 +0900

    added common.js and script for cufon

---
+++ static/javascripts/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	Cufon.replace(".header h1", {
+		color: '-linear-gradient(#999, 0.2=#777, 0.4=#444, 0.5=#222, 0.6=#333, 0.8=#555, #999)'
+	});
+	Cufon.replace(".header h2");
+	Cufon.replace(".header li", {
+		hover: {
+			//textShadow: '2px 2px red',
+			color: '-linear-gradient(black, white)'
+		}
+	});
+});
