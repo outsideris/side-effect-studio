455d73af4e68ef825c5ba8ea2aab81ed9e260e88
Outsider <outsideris@gmail.com>
2010-12-23 03:49:12 +0900

    added some tests for controllers

--- test/server.test.js
+++ test/server.test.js
@@ -4,11 +4,12 @@
 process.env.NODE_ENV = 'test';
 
 var server = require('../server.js'),
+    child_process = require('child_process'),
     assert = require('assert');
     
 module.exports = {
     // static test
-    'test Staticfiles: cufon-yui.js': function() {
+    'test Staticfiles URL: cufon-yui.js': function() {
         assert.response(server, {
             url: '/javascripts/cufon-yui.js',
             method: 'GET'
@@ -16,7 +17,7 @@ module.exports = {
             status: 200
         });
     },
-    'test Staticfiles: Orbitron_900-Orbitron_700.font.js': function() {
+    'test Staticfiles URL: Orbitron_900-Orbitron_700.font.js': function() {
         assert.response(server, {
             url: '/javascripts/Orbitron_900-Orbitron_700.font.js',
             method: 'GET'
@@ -24,7 +25,7 @@ module.exports = {
             status: 200
         });
     },
-    'test Staticfiles: nodejs-badge.png': function() {
+    'test Staticfiles URL: nodejs-badge.png': function() {
         assert.response(server, {
             url: '/images/nodejs-badge.png',
             method: 'GET'
@@ -32,7 +33,7 @@ module.exports = {
             status: 200
         });
     },
-    'test Staticfiles: PoweredMongoDBbrown.png': function() {
+    'test Staticfiles URL: PoweredMongoDBbrown.png': function() {
         assert.response(server, {
             url: '/images/PoweredMongoDBbrown.png ',
             method: 'GET'
@@ -40,7 +41,7 @@ module.exports = {
             status: 200
         });
     },
-    'test Staticfiles: common-style.css': function() {
+    'test Staticfiles URL: common-style.css': function() {
         assert.response(server, {
             url: '/stylesheets/common-style.css',
             method: 'GET'
@@ -48,12 +49,39 @@ module.exports = {
             status: 200
         });
     },
-    'test Staticfiles: REPL-style.css': function() {
+    'test Staticfiles URL: REPL-style.css': function() {
         assert.response(server, {
             url: '/stylesheets/REPL-style.css',
             method: 'GET'
         }, {
             status: 200
+        });
+    },
+    
+    // Controller Test
+    'test root controller URL': function() {
+        assert.response(server, {
+            url: '/',
+            method: 'GET'
+        }, {
+            status: 200
+        });
+    },
+    'test version URL': function() {
+        assert.response(server, {
+            url: '/version',
+            method: 'GET'
+        }, {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        },
+        
+        function(res) {
+            var version = JSON.parse(res.body);
+            child_process.exec('node --version', function(err, stdout, stderr) {
+                if (err) throw err;
+                assert.eql({"nodejs_version":stdout}, version)
+            });
         });
     },
 };
