98cd4f7d3caf1d0003d1474be3e5ab3824a9ef31
Outsider <outsideris@gmail.com>
2010-12-25 13:49:59 +0900

    added test file for repl2

---
+++ test/repl2.test.js
@@ -0,0 +1,11 @@
+// Run $ expresso
+
+// Force test environment
+process.env.NODE_ENV = 'test';
+
+var server = require('../repl2.js'),
+    assert = require('assert');
+    
+module.exports = {
+  
+};
