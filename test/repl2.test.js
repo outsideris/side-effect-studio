1efe24013870d3faf618fb9d610b369968046a21
Outsider <outsideris@gmail.com>
2010-12-25 14:12:51 +0900

    added tests fot stripBrace

--- test/repl2.test.js
+++ test/repl2.test.js
@@ -3,9 +3,15 @@
 // Force test environment
 process.env.NODE_ENV = 'test';
 
-var server = require('../repl2.js'),
+var repl2 = require('../repl2.js'),
     assert = require('assert');
     
 module.exports = {
-  
+    // stripBrace
+    'test stripBrace brace in last': function() {
+        assert.equal('whoami', repl2.stripBrace('whoami()'));
+    },
+    'test stripBrace : brace in middle': function() {
+        assert.equal('test()test', repl2.stripBrace('test()test'));
+    },
 };
