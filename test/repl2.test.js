af4050bb819ba3f63caa78248759893cc6deafb5
Outsider <outsideris@gmail.com>
2010-12-25 14:22:34 +0900

    added tests for userCommand

--- test/repl2.test.js
+++ test/repl2.test.js
@@ -8,10 +8,27 @@ var repl2 = require('../repl2.js'),
     
 module.exports = {
     // stripBrace
-    'test stripBrace brace in last': function() {
+    'test stripBrace : brace in last': function() {
         assert.equal('whoami', repl2.stripBrace('whoami()'));
     },
     'test stripBrace : brace in middle': function() {
         assert.equal('test()test', repl2.stripBrace('test()test'));
+    },
+    
+    // userCommand
+    'test userCommand : whoami()': function() {
+        assert.equal(true, repl2.userCommand('whoami()'));
+    },
+    'test userCommand : contacts()': function() {
+        assert.equal(true, repl2.userCommand('contacts()'));
+    },
+    'test userCommand : projects()': function() {
+        assert.equal(true, repl2.userCommand('projects()'));
+    },
+    'test userCommand : skills()': function() {
+        assert.equal(true, repl2.userCommand('skills()'));
+    },
+    'test userCommand : somethings()': function() {
+        assert.equal(false, repl2.userCommand('shomthins()'));
     },
 };
