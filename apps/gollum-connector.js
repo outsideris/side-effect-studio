400005dc3603f46ff778b39c19ff1b4efbf82f21
Outsider <outsideris@gmail.com>
2011-01-01 23:52:14 +0900

    fixed useing jqueryify

--- apps/gollum-connector.js
+++ apps/gollum-connector.js
@@ -8,8 +8,10 @@ gollum.getContents = function(url, res) {
   request({uri:'http://localhost:4567'+url}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var window = jsdom.jsdom(body).createWindow();
+      
       jsdom.jQueryify(window, 'http://code.jquery.com/jquery-1.4.2.min.js', function (window, jquery) {
-        var gollumContents = window.jQuery('.site').html();
+        var gollumContents = window.$('.site').html();
+        // sys.debug(gollumContents);
         res.render('gollum.jade', {
           locals: {
              title:'wiki',
@@ -19,4 +21,4 @@ gollum.getContents = function(url, res) {
       });
     }
   });
-}
+};
