5d9dacc6f3d31bdd8822f84fd5d60236e648e3b0
Outsider <outsideris@gmail.com>
2011-09-27 11:59:52 +0900

    added cron job for updating Thinkup

--- apps/cron.js
+++ apps/cron.js
@@ -9,9 +9,20 @@ function log(err, stdout, stderr) {
   }
 }
 
+// Gollum auto push
 new cron.CronJob('0 0 * * *', function() {
   process.chdir('/home/rockdoli/www/gollum-wiki/');
   exec('.git/hooks/post-commit', log);
   console.log('Gollum wikin pushed at : ' + new Date);      
 });
 
+// Thinkup update
+new cron.CronJob('0 5 * * *', function() {
+  exec('cd /var/www/apache/thinkup/crawler/;export THINKUP_PASSWORD=' + process.env["THINKUP_PASS"]+ '; /usr/bin/php crawl.php outsideris@gmail.com', log);
+  console.log('Thinkup updated at : ' + new Date);      
+});
+
+new cron.CronJob('0 17 * * *', function() {
+  exec('cd /var/www/apache/thinkup/crawler/;export THINKUP_PASSWORD=' + process.env["THINKUP_PASS"]+ '; /usr/bin/php crawl.php outsideris@gmail.com', log);
+  console.log('Thinkup updated at : ' + new Date);      
+});
