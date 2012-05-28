43fa06c055feb44d3fb37d54d7402852956c7b7a
Outsider <outsideris@gmail.com>
2012-05-28 18:51:35 +0900

    gollum 2.0에 맞게 수정

--- apps/gollum-connector.js
+++ apps/gollum-connector.js
@@ -5,22 +5,22 @@ var util = require('util'),
 
 gollum.getContents = function(pageUrl, res) {
   jsdom.env('http://'+process.env["GOLLUM_HOST"] +':'+ process.env["GOLLUM_PORT"]+pageUrl, function (error, window) {
-    jsdom.jQueryify(window, 'http://code.jquery.com/jquery-1.4.2.min.js', function (window, jquery) {
-      var regex = /(\<a href=\"\/\"\>Home\<\/a\>)\s+\|/g;
-      if (window.$('div.write').html()) {
+    jsdom.jQueryify(window, 'http://code.jquery.com/jquery-1.7.2.min.js', function () {
+      if (window.$('#gollum-editor-submit').val() === 'Save') {
          res.send('', 404);
          return;
       }
         
-      var siteDom = window.$('.site');
-      var title = ' - ' + siteDom.find('h1').first().text();
+      var siteDom = window.$('body');
+      var head = window.$('#head');
+      var title = ' - ' + head.find('h1').first().text();
 
       var gollumContents = siteDom
-                              .find('a[href=/edit'+ pageUrl + ']').remove().end()
-                              .find('a[href=/edit/Home]').remove().end()
+                              .find('#minibutton-new-page').remove().end()
+                              .find('.action-edit-page').remove().end()
+                              .find('#search-query').val('').end()
+                              .find('.gollum-revert-button').remove().end()
                               .html();
-      gollumContents = gollumContents.replace(regex, '$1')
-      gollumContents = gollumContents.replace(/&amp;rarr;/g, '&rarr;');
       
       var pathName = url.parse(pageUrl).pathname;
       var useDisqus = true;
--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -9,4 +9,105 @@ $(document).ready(function() {
 			color: '-linear-gradient(black, white)'
 		}
 	});
+  
+  var nodeSelector = {
+    node1: null,
+    node2: null,
+
+    selectNodeRange: function( n1, n2 ) {
+      if ( nodeSelector.node1 && nodeSelector.node2 ) {
+        $('#wiki-history td.selected').removeClass('selected');
+        nodeSelector.node1.addClass('selected');
+        nodeSelector.node2.addClass('selected');
+
+        // swap the nodes around if they went in reverse
+        if ( nodeSelector.nodeComesAfter( nodeSelector.node1,
+              nodeSelector.node2 ) ) {
+                var n = nodeSelector.node1;
+                nodeSelector.node1 = nodeSelector.node2;
+                nodeSelector.node2 = n;
+              }
+
+        var s = true;
+        var $nextNode = nodeSelector.node1.next();
+        while ( $nextNode ) {
+          $nextNode.addClass('selected');
+          if ( $nextNode[0] == nodeSelector.node2[0] ) {
+            break;
+          }
+          $nextNode = $nextNode.next();
+        }
+      }
+    },
+
+    nodeComesAfter: function ( n1, n2 ) {
+      var s = false;
+      $(n1).prevAll().each(function() {
+        if ( $(this)[0] == $(n2)[0] ) {
+          s = true;
+        }
+      });
+      return s;
+    },
+
+    checkNode: function( nodeCheckbox ) {
+      var $nodeCheckbox = nodeCheckbox;
+      var $node = $(nodeCheckbox).parent().parent();
+      // if we're unchecking
+      if ( !$nodeCheckbox.is(':checked') ) {
+
+        // remove the range, since we're breaking it
+        $('#wiki-history tr.selected').each(function() {
+          if ( $(this).find('td.checkbox input').is(':checked') ) {
+            return;
+          }
+          $(this).removeClass('selected');
+        });
+
+        // no longer track this
+        if ( $node[0] == nodeSelector.node1[0] ) {
+          nodeSelector.node1 = null;
+          if ( nodeSelector.node2 ) {
+            nodeSelector.node1 = nodeSelector.node2;
+            nodeSelector.node2 = null;
+          }
+        } else if ( $node[0] == nodeSelector.node2[0] ) {
+          nodeSelector.node2 = null;
+        }
+
+      } else {
+        if ( !nodeSelector.node1 ) {
+          nodeSelector.node1 = $node;
+          nodeSelector.node1.addClass('selected');
+        } else if ( !nodeSelector.node2 ) {
+          // okay, we don't have a node 2 but have a node1
+          nodeSelector.node2 = $node;
+          nodeSelector.node2.addClass('selected');
+          nodeSelector.selectNodeRange( nodeSelector.node1,
+              nodeSelector.node2 );
+        } else {
+          // we have two selected already
+          $nodeCheckbox[0].checked = false;
+        }
+      }
+    }
+  };
+
+  if ($('#wiki-wrapper').hasClass('history')) {
+    $('#wiki-history td.checkbox input').each(function() {
+      $(this).click(function() {
+        nodeSelector.checkNode($(this));
+      });
+      if ( $(this).is(':checked') ) {
+        nodeSelector.checkNode($(this));
+      }
+    });
+
+    if ($('.history a.action-compare-revision').length) {
+      $('.history a.action-compare-revision').click(function() {
+        $("#version-form").submit();
+      });
+    }
+  }
+
 });
